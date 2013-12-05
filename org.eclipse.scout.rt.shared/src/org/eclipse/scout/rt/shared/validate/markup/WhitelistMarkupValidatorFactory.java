b57319e0b98f012ddc2d9a0f0a2bb3bc70f80e57
Ken Lee <kle@bsiag.com>
2013-12-05 11:48:32 +0100

    Adds MarkupValidator factory.

---
+++ org.eclipse.scout.rt.shared/src/org/eclipse/scout/rt/shared/validate/markup/WhitelistMarkupValidatorFactory.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.shared.validate.markup;
+
+/**
+ * @since 3.10.0-M4
+ */
+public class WhitelistMarkupValidatorFactory implements IMarkupValidatorFactory {
+
+  @Override
+  public IMarkupValidator createMarkupValidator() {
+    return new WhitelistMarkupValidator();
+  }
+}
